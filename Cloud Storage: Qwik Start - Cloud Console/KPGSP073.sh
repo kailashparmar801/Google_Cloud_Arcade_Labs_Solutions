b02a7d571f1886a7a7f4a3b79ab144e61987d7ac
Kailash Parmar <94098556+kailashparmar801@users.noreply.github.com>
2025-07-28 21:05:48 +0530

    Add files via upload

---
+++ Cloud Storage: Qwik Start - Cloud Console/KPGSP073.sh
@@ -0,0 +1,16 @@
+
+gcloud auth list
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+gsutil mb -l $REGION -c Standard gs://$PROJECT_ID
+
+curl -O https://github.com/Techcps/Google-Cloud-Skills-Boost/blob/main/Cloud%20Storage%3A%20Qwik%20Start%20-%20Cloud%20Console/kitten.png
+
+gsutil cp kitten.png gs://$PROJECT_ID/kitten.png
+
+gsutil iam ch allUsers:objectViewer gs://$PROJECT_ID
+
+
