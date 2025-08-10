126677399d71d41d346cf97b6332222e370ba457
Kailash Parmar <94098556+kailashparmar801@users.noreply.github.com>
2025-08-10 20:51:16 +0530

    Update KPGSP1024.sh

--- Using Prometheus for Monitoring on Google Cloud: Qwik Start/KPGSP1024.sh
+++ Using Prometheus for Monitoring on Google Cloud: Qwik Start/KPGSP1024.sh
@@ -3,11 +3,37 @@ gcloud auth list
 
 export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 
+gcloud artifacts repositories create docker-repo --repository-format=docker \ --location=$LOCATION --description="Docker repository" \ --project=$PROJECT_ID
+
+wget https://storage.googleapis.com/spls/gsp1024/flask_telemetry.zip unzip flask_telemetry.zip docker load -i flask_telemetry.tar
+
+docker tag gcr.io/ops-demo-330920/flask_telemetry:61a2a7aabc7077ef474eb24f4b69faeab47deed9 \ us-west1-docker.pkg.dev/$PROJECT_ID/docker-repo/flask-telemetry:v1
+
+docker push us-west1-docker.pkg.dev/$PROJECT_ID/docker-repo/flask-telemetry:v1
+
 gcloud beta container clusters create gmp-cluster --num-nodes=1 --zone $ZONE --enable-managed-prometheus
 
 gcloud container clusters get-credentials gmp-cluster --zone $ZONE
 
 kubectl create ns gmp-test
+
+wget https://storage.googleapis.com/spls/gsp1024/gmp_prom_setup.zip unzip gmp_prom_setup.zip cd gmp_prom_setup
+
+nano flask_deployment.yaml
+
+$LOCATION-docker.pkg.dev/$PROJECT_ID/docker-repo/flask-telemetry:v1
+
+kubectl -n gmp-test apply -f flask_deployment.yaml
+
+kubectl -n gmp-test apply -f flask_service.yaml
+
+url=$(kubectl get services -n gmp-test -o jsonpath='{.items[*].status.loadBalancer.ingress[0].ip}')
+
+curl $url/metrics
+
+kubectl -n gmp-test apply -f prom_deploy.yaml
+
+timeout 120 bash -c -- 'while true; do curl $(kubectl get services -n gmp-test -o jsonpath='{.items[*].status.loadBalancer.ingress[0].ip}'); sleep $((RANDOM % 4)) ; done'
 
 kubectl -n gmp-test apply -f https://raw.githubusercontent.com/kyleabenson/flask_telemetry/master/gmp_prom_setup/flask_deployment.yaml
 
