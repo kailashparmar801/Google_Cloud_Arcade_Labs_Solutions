7f6248cc41e8ed7442c30345c036cba2ef860497
Kailash Parmar <94098556+kailashparmar801@users.noreply.github.com>
2025-08-23 00:30:01 +0530

    Add files via upload

---
+++ It Speaks Create Synthetic Speech Using Text-to-Speech/KPGSP222.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+
+
+echo
+echo -e "${CYAN}${BOLD}================================================${RESET}"
+echo -e "${YELLOW}${BOLD}             Arcade Labs Solution             ${RESET}"
+echo -e "${CYAN}${BOLD}================================================${RESET}"
+echo
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Progress${RESET}"
+
+
+
+gcloud auth list
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+gcloud services enable texttospeech.googleapis.com
+
+sudo apt-get install -y virtualenv
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+gcloud iam service-accounts create tts-qwiklab
+
+gcloud iam service-accounts keys create tts-qwiklab.json --iam-account tts-qwiklab@$PROJECT_ID.iam.gserviceaccount.com
+
+export GOOGLE_APPLICATION_CREDENTIALS=tts-qwiklab.json
+
+
+
+
+echo "${GREEN}${BOLD}Congratulations:- ${RESET}" "${WHITE}${BOLD}Setup Completed !!!${RESET}"
+
+
+echo
+echo -e "${CYAN}${BOLD}================================================${RESET}"
+echo -e "${YELLOW}${BOLD}           Subscribe To Arcade Labs           ${RESET}"
+echo -e "${CYAN}${BOLD}================================================${RESET}"
+echo
